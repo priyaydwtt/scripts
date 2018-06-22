f2f2f00ac952ef41810c9813d7dd65c3543f8c50
sp <priya.s147@gmail.com>
2018-06-22 20:21:14 +0530

    new test file

---
+++ scripts/exception/exception_sample.py
@@ -0,0 +1,37 @@
+# 1. Exception Handling
+"""
+1. try
+2. except
+3. finally
+"""
+
+# Simple try-except block
+personal_details = {"name":"csk","age":25}
+
+# Error - KeyError
+#print(personal_details["city"])
+
+try:
+    print(personal_details["city"])
+
+except(KeyError,AttributeError) as err:
+    print("value of err ",err)
+    personal_details["city"] = "Bangalore"
+    print("Exception Block")
+    print(personal_details["city"])
+except:
+    print("Error")
+    #pass
+
+# try-except-finally block
+
+print("try-except-finally block")
+try:
+    print(personal_details["city"])
+
+except(KeyError):
+    personal_details["city"] = "Bangalore"
+    print("Exception Block")
+    print(personal_details["city"])
+finally:
+    print("Finally")
--- scripts/looping/loop.py
+++ scripts/looping/loop.py
@@ -3,7 +3,51 @@
 
 price_list= []
 
+print("for loop")
 for i in range(0,100,1):
     price_list = price_list + [i]
 
 print("value of price_ls" +  str(price_list))
+
+
+#while loop
+
+price_list=[]
+#initialization : 0 is like i=0
+
+i=0
+print("while loop")
+
+while(i<100):
+
+    price_list= price_list + [i]
+
+    i=i+1
+
+
+print(price_list)
+# break statement come out of the loop
+
+
+price_list=[]
+
+i=0
+
+print("continue loop")
+
+while(i<100):
+    price_list=price_list + [i]
+    i=i+1
+    if (i==50):
+        print("Continue")
+        pass
+
+print(price_list)
+
+
+
+
+# pass is like "continue" statement in Java skips the iteration and continues to be in the loop
+
+
+
